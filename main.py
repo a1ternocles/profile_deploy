59395ef6e8abd0e668bb9f086d2f4bb83c1a3762
a1ternocles <anmruizto@gmail.com>
2024-03-17 19:16:44 -0500

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as com
-    
+
 
 def load_pdf(filename):
     with open(filename, "rb") as file:
